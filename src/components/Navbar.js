1a7602eec775124c32df32217a53832053f77ca3
claudimar <claudimards@gmail.com>
2020-12-07 19:14:53 -0300

    Estrutura inicial do projeto

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link to='/' className="navbar-logo">
+                        LAVISH
+                    </Link>
+                    <div className="menu-icon">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
