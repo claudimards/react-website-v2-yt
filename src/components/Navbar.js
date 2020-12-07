d8551947736e9b2660fd3ab5274c6bd208c75377
claudimar <claudimards@gmail.com>
2020-12-07 20:51:54 -0300

    Creating the button component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,25 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MdFingerprint } from 'react-icons/md'
 import { FaBars, FaTimes } from 'react-icons/fa'
+import { Button } from './Button'
 
 function Navbar() {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
+
     const handleClick = () => setClick(!click)
+    // eslint-disable-next-line
+    const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false)
+        }else{
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
@@ -21,6 +36,23 @@ function Navbar() {
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item">
                             <Link to="/" className="nav-links">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/services" className="nav-links">Services</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/products" className="nav-links">Products</Link>
+                        </li>
+                        <li className="nav-btn">
+                            {button ? (
+                                <Link to="/sign-up" className="btn-link">
+                                    <Button buttonStyle='btn--outline'>SIGN UP</Button>
+                                </Link>
+                            ) : (
+                                <Link to="/sign-up" className="btn-link">
+                                    <Button buttonStyle='btn--outline' buttonSize='btn--mobile'></Button>
+                                </Link>
+                            )}
                         </li>
                     </ul>
                 </div>
