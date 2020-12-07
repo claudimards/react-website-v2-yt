718a9601dcf60b0e309265c7b4ab2829c1a7eccf
claudimar <claudimards@gmail.com>
2020-12-07 19:45:45 -0300

    Adding React icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { MdFingerprint } from 'react-icons/md'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function Navbar() {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
     return (
         <>
             <div className="navbar">
                 <div className="navbar-container container">
                     <Link to='/' className="navbar-logo">
+                        <MdFingerprint className="navbar-icon" />
                         LAVISH
                     </Link>
-                    <div className="menu-icon">
-
+                    <div className="menu-icon" onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars />}
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links">Home</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </>
