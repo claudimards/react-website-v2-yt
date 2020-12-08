1964e4bc8b90f5027c2d2f2146ec336b933b6a1e
claudimar <claudimards@gmail.com>
2020-12-07 22:01:41 -0300

    Finished navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MdFingerprint } from 'react-icons/md'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { Button } from './Button'
+import './Navbar.css'
+import { IconContext } from 'react-icons/lib'
 
 function Navbar() {
     const [click, setClick] = useState(false)
     const [button, setButton] = useState(true)
 
     const handleClick = () => setClick(!click)
-    // eslint-disable-next-line
     const closeMobileMenu = () => setClick(false)
 
     const showButton = () => {
@@ -20,13 +21,18 @@ function Navbar() {
         }
     }
 
+    useEffect(() => {
+        showButton()
+    }, [])
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
+        <IconContext.Provider value={{color: '#FFF'}}>
             <div className="navbar">
                 <div className="navbar-container container">
-                    <Link to='/' className="navbar-logo">
+                    <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
                         <MdFingerprint className="navbar-icon" />
                         LAVISH
                     </Link>
@@ -35,13 +41,13 @@ function Navbar() {
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item">
-                            <Link to="/" className="nav-links">Home</Link>
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/services" className="nav-links">Services</Link>
+                            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>Services</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/products" className="nav-links">Products</Link>
+                            <Link to="/products" className="nav-links" onClick={closeMobileMenu}>Products</Link>
                         </li>
                         <li className="nav-btn">
                             {button ? (
@@ -49,14 +55,15 @@ function Navbar() {
                                     <Button buttonStyle='btn--outline'>SIGN UP</Button>
                                 </Link>
                             ) : (
-                                <Link to="/sign-up" className="btn-link">
-                                    <Button buttonStyle='btn--outline' buttonSize='btn--mobile'></Button>
+                                <Link to="/sign-up" className="btn-link" onClick={closeMobileMenu}>
+                                    <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>SIGN UP</Button>
                                 </Link>
                             )}
                         </li>
                     </ul>
                 </div>
             </div>
+        </IconContext.Provider>
         </>
     )
 }
